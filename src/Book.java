4ddf011cde01b58fb64b86955d1d2348a5a51000
sachetwasti1997 <sachetwasti61@gmail.com>
2023-09-10 11:33:45 +0530

    Project Initialisation

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,32 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Book {
+    private final int id;
+    private final Lock lock;
+
+    public Book(int id) {
+        this.id = id;
+        lock = new ReentrantLock();
+    }
+
+    public void readBook(Student student) throws InterruptedException {
+        try {
+            if (lock.tryLock(10, TimeUnit.MINUTES)) {
+                System.out.println(student+" has started reading "+this);
+                Thread.sleep(2000);
+            }
+        } finally {
+            lock.unlock();
+            System.out.println(student+" has just finished reading "+this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                '}';
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,33 @@
-public class Student {
+import java.util.Random;
+
+public class Student implements Runnable{
+
+    private final int id;
+    private final Random random;
+    private final Book[] books;
+
+    public Student(int id, Book[] books) {
+        this.id = id;
+        random = new Random();
+        this.books = books;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            int bookId = random.nextInt(0, Constants.NUM_OF_BOOKS);
+            try {
+                books[bookId].readBook(this);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                '}';
+    }
 }
